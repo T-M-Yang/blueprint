02a786b8cfb486af4ea3f98e7c0e7ed9d7e97bd1
Young <ytm199891@gmail.com>
2023-03-15 16:11:10 +0800

    New project Started! 	new file:   components/Mobile.jsx 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/index.js 	new file:   postcss.config.js 	deleted:    public/favicon.ico 	deleted:    public/next.svg 	deleted:    public/thirteen.svg 	deleted:    public/vercel.svg 	modified:   styles/globals.css 	new file:   tailwind.config.js

---
+++ components/Mobile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Mobile = () => {
+  return (
+    <section className="grid">
+      <div className="w-full h-screen xs:w-2/3 xs:min-h-2/3 xs:max-h-10 bg-slate-700"></div>
+    </section>
+  );
+};
+
+export default Mobile;
